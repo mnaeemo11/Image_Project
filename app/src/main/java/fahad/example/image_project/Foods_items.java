07b87c348248cdb68a52ff23117d0d59e8fab3d2
mnaee <mnaeem67@gmail.com>
2022-11-28 22:11:40 +0500

    Initial commit

---
+++ app/src/main/java/fahad/example/image_project/Foods_items.java
@@ -0,0 +1,14 @@
+package fahad.example.image_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Foods_items extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foods_items);
+    }
+}
---
+++ app/src/main/java/fahad/example/image_project/update_food.java
@@ -0,0 +1,14 @@
+package fahad.example.image_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class update_food extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_food);
+    }
+}
